0654bee02645cec533445a1160468508ace26757
cdevelopment010 <cdevelopment010@gmail.com>
2022-04-12 12:44:15 +0100

    initial commit

---
+++ Rscript.R
@@ -0,0 +1,79 @@
+##### Task break down #####
+
+# 1) create variables to load different data files in the future
+# 2) manipulate file to create required DB table structure
+# 3) push data to SQL DB
+
+
+## assumptions
+# - new files will be of the same format and layout, i.e use Grid-ref
+# - No duplicates within the file provided
+# - [SQL side] no duplicates across different files
+# - using local mySQL database
+
+
+## improvements
+# - for loop is very slow, and I don't like looping for a number of years. It would be better to loop until the next Grid-ref
+# - Obviously password in file isn't great!
+
+
+
+####### load required packages ########
+install.packages("RMySQL", dependencies=TRUE)
+library(RMySQL)
+
+
+###### Set variables ######
+# Variables for file name, SQL connection etc.
+filename <- 'cru-ts-2-10.1991-2000-cutdown.pre' 
+fileLoc <- './data'
+fileToRead <- paste(fileLoc,"/", filename, sep="")
+
+
+
+
+##### Import Data ##########
+#raw data
+data <- read.table(fileToRead, sep=",", as.is=FALSE, header=FALSE, fill=TRUE)
+
+#final data frame that will be pushed to SQL
+df <- data.frame(matrix(ncol = 4, nrow = 0))
+cols <- c("xref", "yref", "date", "value")
+colnames(df) <- cols
+
+# populate df
+for (i in 1:nrow(data)){
+  if (substr(data[i,1], 1, 8) == 'Grid-ref'){
+    xref <- scan(text = as.character(data[i,1]), what="=", quiet = TRUE)[2]
+    yref <- as.character(data[i,2])
+    
+    #next 10 rows indicate year
+    for (j in 1:10){
+      #each row is a year, and space separated values are the values per month
+      month <- data.frame()
+      month <- scan(text = as.character(data[i+j, 1]), what = "", quiet = TRUE)
+      #next loop for months
+      for (k in 1:12){
+        date <- paste(k,"/1/",1990+j,sep="")
+        value <- month[k]
+        dfRow <- cbind(xref, yref, date, value)
+        df <- rbind(df, dfRow)
+      }
+    }
+  }
+}
+
+
+
+##### MySQL DB connection #####
+
+mydb = dbConnect(MySQL(), user='root', password='P@55w0rD', dbname='codeChallenge', host='craig-HP-Compaq-Elite-8300-CMT')
+dbListTables(mydb)
+dbWriteTable(mydb, "rainfall",data.frame(df),row.names = FALSE, append = TRUE)
+dbReadTable(mydb, "rainfall")
+
+
+
+
+##### Clear environment #####
+rm(list = ls())
---
+++ brief/brief.md
@@ -0,0 +1,20 @@
+# Code Challenge JBA
+
+At JBA we regularly receive varied datasets from our clients. To work with them efficiently, we often have to transform them into something which we can use more readily. We want to see your development skills in data manipulation.
+
+The example data contains precipitation (rainfall) data that we would like in a database table with the following structure.
+
+| Xref	| Yref | Date | Value |
+|------|------|-----|-----|
+| 1	| 148 | 1/1/1991 | 3020 |
+| 1 | 148 | 2/1/1991 | 2820 |
+
+
+To help you understand the data, an excerpt is shown below with notes on its structure.
+
+![](./code-challenge-example.png)
+
+The code that you write should have an option to specify the file name, read in the header data, transform the data, create the database table structure and insert the information into the database.
+
+You are free to use any programming language and database technology that you choose. For junior developers we're looking to see what you can do; for senior developers we want to see evidence of your coding standards.
+
