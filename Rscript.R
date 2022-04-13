aa535b1837ccf6afda6b58a6f47fc8291418dc38
cdevelopment010 <cdevelopment010@gmail.com>
2022-04-13 17:39:52 +0100

    Feat: replace current file with copy

--- Rscript.R
+++ Rscript.R
@@ -1,5 +1,4 @@
 ##### Task break down #####
-
 # 1) create variables to load different data files in the future
 # 2) manipulate file to create required DB table structure
 # 3) push data to SQL DB
@@ -13,7 +12,7 @@
 
 
 ## improvements
-# - for loop is extremely(!) slow, and I don't like looping for a number of years. It would be better to loop until the next Grid-ref
+# - issue with SQL sometimes "Loading local data is disabled"
 # - Obviously password in file isn't great!
 
 
@@ -21,7 +20,10 @@
 ####### load required packages ########
 install.packages("RMySQL", dependencies=TRUE)
 library(RMySQL)
-
+install.packages('data.table')
+library(data.table)
+install.packages('dplyr')
+library(dplyr)
 
 ###### Set variables ######
 # Variables for file name, SQL connection etc.
@@ -31,49 +33,73 @@ fileToRead <- paste(fileLoc,"/", filename, sep="")
 
 
 
-
 ##### Import Data ##########
 #raw data
-data <- read.table(fileToRead, sep=",", as.is=FALSE, header=FALSE, fill=TRUE)
+#data <- read.table(fileToRead, sep=",", as.is=FALSE, header=FALSE, fill=TRUE)
+data2 <- read.table(text = gsub(",", "\t", readLines(fileToRead)), fill=TRUE) #this loads the data in a better format
 
-#final data frame that will be pushed to SQL
-df <- data.frame(matrix(ncol = 4, nrow = 0))
+#head information from file
+data2Header <- data.frame(data2)[c(1,2,3,4,5),]
+#remove head information leaving just data.
+data <- data.frame(data2)[-c(1,2,3,4,5),]
+
+##Using year within the loop - I don't like how I have specified a specific cell for years. 
+years <-substring(data2Header[5,3],8,16)
+years <- data.frame(strsplit(years,"-"))
+
+####final data frame that will be pushed to SQL
+#df <- data.frame(matrix(ncol = 4, nrow = 0))
+
+## prepopulate with approx rows to try to speed up loop
+df <- data.frame(matrix(ncol = 4, nrow = nrow(data)*12)) #approx length
 cols <- c("xref", "yref", "date", "value")
 colnames(df) <- cols
 
 #progress bar
-pb = txtProgressBar(min = 0, max = nrow(data), initial = 0) 
+pb = txtProgressBar(min = 0, max = nrow(data), initial = 0, style = 3) 
+#pb = txtProgressBar(min = 0, max = 100, initial = 0, style = 3) 
+
 
 # populate df
+currRow <- 1
+start_time <- Sys.time()
 for (i in 1:nrow(data)){
+#for (i in 1:100){
   setTxtProgressBar(pb,i)
   if (substr(data[i,1], 1, 8) == 'Grid-ref'){
-    xref <- scan(text = as.character(data[i,1]), what="=", quiet = TRUE)[2]
-    yref <- as.character(data[i,2])
-    
-    #next 10 rows indicate year
-    for (j in 1:10){
-      #each row is a year, and space separated values are the values per month
-      month <- data.frame()
-      month <- scan(text = as.character(data[i+j, 1]), what = "", quiet = TRUE)
-      #next loop for months
-      for (k in 1:12){
-        date <- paste(k,"/1/",1990+j,sep="")
-        value <- month[k]
-        dfRow <- cbind(xref, yref, date, value)
-        df <- rbind(df, dfRow)
-      }
-    }
+    curryear <- as.numeric(years[1,])
+    xref <- as.character(data[i,2])
+    yref <- as.character(data[i,3])
+    next
   }
+  for (k in 1:12){
+    date <- paste(k,"/1/",curryear,sep="")
+    value <- data[i,k]
+    dfRow <- c(xref, yref, date, value)
+    df[currRow,] <- dfRow
+    currRow <- currRow + 1
+  }
+  curryear <- curryear+1
+  if ( i %% 1000 == 0) {
+    print(paste("Number of records processed ", i))
+  }  
 }
+
 close(pb)
 
+end_time <- Sys.time()
+end_time - start_time
+## 1.5 hours.... not great but still a lot better than before! 
+
+## remove empty rows due to over estimating data frame size
+df2 <- df[rowSums(is.na(df)) != ncol(df),]
+#62K difference
 
 ##### MySQL DB connection #####
 
 mydb = dbConnect(MySQL(), user='root', password='P@55w0rD', dbname='codeChallenge', host='craig-HP-Compaq-Elite-8300-CMT')
 dbListTables(mydb)
-dbWriteTable(mydb, "rainfall",data.frame(df),row.names = FALSE, append = TRUE)
+dbWriteTable(mydb, "rainfall",data.frame(df2),row.names = FALSE, append = TRUE) #getting the occasional message about loading local data must be enabled.
 dbReadTable(mydb, "rainfall")
 
 
@@ -81,3 +107,5 @@ dbReadTable(mydb, "rainfall")
 
 ##### Clear environment #####
 rm(list = ls())
+
+
