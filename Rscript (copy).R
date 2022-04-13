ac6a03be51eeef2dee97bd031fe3a5716d4a1e5a
cdevelopment010 <cdevelopment010@gmail.com>
2022-04-13 17:36:15 +0100

    Feat: change for loop to improve speed

--- Rscript (copy).R
+++ Rscript (copy).R
@@ -1,5 +1,4 @@
 ##### Task break down #####
-getwd()
 # 1) create variables to load different data files in the future
 # 2) manipulate file to create required DB table structure
 # 3) push data to SQL DB
@@ -13,7 +12,7 @@ getwd()
 
 
 ## improvements
-# - for loop is extremely(!) slow, and I don't like looping for a number of years. It would be better to loop until the next Grid-ref
+# - issue with SQL sometimes "Loading local data is disabled"
 # - Obviously password in file isn't great!
 
 
@@ -23,6 +22,8 @@ install.packages("RMySQL", dependencies=TRUE)
 library(RMySQL)
 install.packages('data.table')
 library(data.table)
+install.packages('dplyr')
+library(dplyr)
 
 ###### Set variables ######
 # Variables for file name, SQL connection etc.
@@ -35,34 +36,35 @@ fileToRead <- paste(fileLoc,"/", filename, sep="")
 ##### Import Data ##########
 #raw data
 #data <- read.table(fileToRead, sep=",", as.is=FALSE, header=FALSE, fill=TRUE)
-data2 <- read.table(text = gsub(",", "\t", readLines(fileToRead)), fill=TRUE) #This works sort of...
+data2 <- read.table(text = gsub(",", "\t", readLines(fileToRead)), fill=TRUE) #this loads the data in a better format
 
+#head information from file
 data2Header <- data.frame(data2)[c(1,2,3,4,5),]
+#remove head information leaving just data.
 data <- data.frame(data2)[-c(1,2,3,4,5),]
+
+##Using year within the loop - I don't like how I have specified a specific cell for years. 
 years <-substring(data2Header[5,3],8,16)
 years <- data.frame(strsplit(years,"-"))
 
+####final data frame that will be pushed to SQL
+#df <- data.frame(matrix(ncol = 4, nrow = 0))
 
-
-
-#final data frame that will be pushed to SQL
-df <- data.frame(matrix(ncol = 4, nrow = 0))
+## prepopulate with approx rows to try to speed up loop
+df <- data.frame(matrix(ncol = 4, nrow = nrow(data)*12)) #approx length
 cols <- c("xref", "yref", "date", "value")
 colnames(df) <- cols
 
 #progress bar
 pb = txtProgressBar(min = 0, max = nrow(data), initial = 0, style = 3) 
-#pb = txtProgressBar(min = 0, max = 10000, initial = 0, style = 3) 
+#pb = txtProgressBar(min = 0, max = 100, initial = 0, style = 3) 
 
-
-## UPDATE CODE AFTER FINISHING - data.table::rbindlist() is 10x faster than rbind
-## OR DPLYR AND BIND_ROWS
-## The problem is that rbind slows down the larger the file becomes
 
 # populate df
+currRow <- 1
 start_time <- Sys.time()
 for (i in 1:nrow(data)){
-#for (i in 1:10000){
+#for (i in 1:100){
   setTxtProgressBar(pb,i)
   if (substr(data[i,1], 1, 8) == 'Grid-ref'){
     curryear <- as.numeric(years[1,])
@@ -71,26 +73,33 @@ for (i in 1:nrow(data)){
     next
   }
   for (k in 1:12){
-        date <- paste(k,"/1/",curryear,sep="")
-        value <- data[i,k]
-        dfRow <- cbind(xref, yref, date, value)
-        df <- data.table::rbindlist(df, dfRow)
+    date <- paste(k,"/1/",curryear,sep="")
+    value <- data[i,k]
+    dfRow <- c(xref, yref, date, value)
+    df[currRow,] <- dfRow
+    currRow <- currRow + 1
   }
   curryear <- curryear+1
   if ( i %% 1000 == 0) {
     print(paste("Number of records processed ", i))
   }  
 }
+
 close(pb)
 
 end_time <- Sys.time()
 end_time - start_time
+## 1.5 hours.... not great but still a lot better than before! 
+
+## remove empty rows due to over estimating data frame size
+df2 <- df[rowSums(is.na(df)) != ncol(df),]
+#62K difference
 
 ##### MySQL DB connection #####
 
 mydb = dbConnect(MySQL(), user='root', password='P@55w0rD', dbname='codeChallenge', host='craig-HP-Compaq-Elite-8300-CMT')
 dbListTables(mydb)
-dbWriteTable(mydb, "rainfall",data.frame(df),row.names = FALSE, append = TRUE)
+dbWriteTable(mydb, "rainfall",data.frame(df2),row.names = FALSE, append = TRUE) #getting the occasional message about loading local data must be enabled.
 dbReadTable(mydb, "rainfall")
 
 
@@ -98,3 +107,5 @@ dbReadTable(mydb, "rainfall")
 
 ##### Clear environment #####
 rm(list = ls())
+
+
